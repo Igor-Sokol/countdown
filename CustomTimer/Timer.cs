20cad483669a58a3ef1270d8477e86a506ceabf4
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2021-01-26 18:58:22 +0300

    initial-commit

---
+++ CustomTimer/Timer.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomTimer
+{
+    /// <summary>
+    /// A custom class for simulating a countdown clock, which implements the ability to send a messages and additional
+    /// information about the Started, Tick and Stopped events to any types that are subscribing the specified events.
+    /// 
+    /// - When creating a CustomTimer object, it must be assigned:
+    ///     - name (not null or empty string, otherwise ArgumentException will be thrown);
+    ///     - the number of ticks (the number must be greater than 0 and less than 10, otherwise an exception
+    ///     will throw an ArgumentException).
+    /// 
+    /// - After the timer has been created, it should fire the Started event, the event should contain information about
+    /// the name of the timer and the number of ticks to start.
+    /// 
+    /// - After starting the timer, it fires Tick events, which contain information about the name of the timer and
+    /// the number of ticks left for triggering, there should be delays between Tick events, delays are modeled by Thread.Sleep.
+    /// 
+    /// - After all Tick events are triggered, the timer should start the Stopped event, the event should contain information about
+    /// the name of the timer.
+    /// </summary>
+    public class Timer
+    {
+    }
+}
